02f421b379503037543a4f41c231fbb5f7155abf
natogourmet <natogourmet@hotmail.com>
2020-04-21 19:40:02 -0500

    challenge-21.1

--- 21/index.js
+++ 21/index.js
@@ -1,7 +1,11 @@
 const arrow = document.querySelector('.outter-circle');
+const imgs = document.querySelectorAll('.pokemon');
 
 navigator.geolocation.watchPosition((data) => {
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+    imgs.forEach(img => {
+        img.style.transform = `rotate(-${data.coords.heading}deg)`;
+    });
 }, (err) => {
     console.error(err);
 });
